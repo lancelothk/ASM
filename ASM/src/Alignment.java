5bbc0cd1daca7276950901e7bf128c185f193a01
Ke Hu <me@huke.info>
2014-02-08 19:58:24 -0500

    finished alignemnt function

--- ASM/src/Alignment.java
+++ ASM/src/Alignment.java
@@ -1,24 +1,32 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
 public class Alignment {
 	public static void main(String[] args) {
-		
+		String targetFileName = "/media/ke/win-data/Dataset/WholeGenomeMethylation/CPGI_chr6_cpg35";
+		String outputFileName = targetFileName + ".aligned";
+		List<Read> readsList = readMappedReads(targetFileName);
+		alignReads(readsList, outputFileName);
 	}
-	
-	public static ArrayList<Read> readMappedReads(String fileName){
+
+	public static ArrayList<Read> readMappedReads(String inputFileName) {
 		ArrayList<Read> readsList = new ArrayList<>();
+		BufferedReader bufferedReader;
 		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-			String line=null;
-			while ((line =bufferedReader.readLine()) != null){
+			bufferedReader = new BufferedReader(new FileReader(inputFileName));
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null) {
 				String[] items = line.split("\t");
-				readsList.add(new Read(items[0],items[1],Long.parseLong(items[2]), Long.parseLong(items[3]),items[4]));
+				if (!items[1].equals("+") && !items[1].equals("-")){
+					System.err.println("invalid strand symbol!");
+				}
+				readsList.add(new Read(items[0], items[1].charAt(0), Long.parseLong(items[2]), Long.parseLong(items[3]), items[4], items[5]));
 			}
 			bufferedReader.close();
 		} catch (IOException e) {
@@ -27,10 +35,23 @@ public class Alignment {
 		}
 		return readsList;
 	}
-	
-	public static void alignReads(List<Read> readsList){
+
+	public static void alignReads(List<Read> readsList, String outputFileName) {
 		// sort reads first
 		Collections.sort(readsList, new ReadComparator());
-		
+		// set initial position
+		long initialPos = readsList.get(0).getStart();
+		BufferedWriter bufferedWriter;
+		try {
+			bufferedWriter = new BufferedWriter(new FileWriter(outputFileName));
+			for (Read read : readsList) {
+				bufferedWriter.write(read.toString(initialPos) + "\n");
+			}
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- ASM/src/Read.java
+++ ASM/src/Read.java
@@ -1,31 +1,77 @@
+import org.apache.commons.lang3.StringUtils;
 
 public class Read {
 	private String chr;
-	private String strand;
+	private char strand;
 	private long start;
 	private long end;
 	private String content;
-	public Read(String chr, String strand, long start, long end, String content) {
+	private String id;
+
+	public Read(String chr, char strand, long start, long end, String content, String id) {
 		super();
 		this.chr = chr;
 		this.strand = strand;
 		this.start = start;
 		this.end = end;
 		this.content = content;
+		this.id = id;
 	}
+
 	public String getChr() {
 		return chr;
 	}
-	public String getStrand() {
+
+	public char getStrand() {
 		return strand;
 	}
+
 	public long getStart() {
 		return start;
 	}
+
 	public long getEnd() {
 		return end;
 	}
+
 	public String getContent() {
 		return content;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public String getComplementaryContent(){
+		StringBuilder stringBuilder = new StringBuilder();
+		for (char c : content.toCharArray()) {
+			switch (c) {
+			case 'A':
+				c = 'T';
+				break;
+			case 'T':
+				c = 'A';
+				break;
+			case 'C':
+				c = 'G';
+				break;
+			case 'G':
+				c = 'C';
+				break;
+			default:
+				break;
+			}
+			stringBuilder.append(c);
+		}
+		return stringBuilder.toString();
+	}
+
+	public String toString(long initialPos) throws Exception {
+		if (this.start - initialPos > Integer.MAX_VALUE) {
+			throw new Exception("offset exceed max int!");
+		}
+		int offset = (int) (this.start - initialPos);
+		return String.format("%s\t%s\t%d\t%d\t%s\t%s", this.chr, this.strand, this.start, this.end,
+				StringUtils.leftPad(this.strand == '+'?this.content:getComplementaryContent(), offset + this.content.length(), "."), this.id);
 	}
 }
