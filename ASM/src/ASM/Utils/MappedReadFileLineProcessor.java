61aa508dcc9c9e7b2b2659d526c8a39f78122da9
Ke Hu <me@huke.info>
2014-02-21 18:07:24 -0500

    refactor MappedReadFileLineProcessor

--- ASM/src/ASM/Utils/MappedReadFileLineProcessor.java
+++ ASM/src/ASM/Utils/MappedReadFileLineProcessor.java
@@ -14,18 +14,19 @@ import java.util.List;
  * Implemented LineProcessor for reading MappedRead File
  */
 public class MappedReadFileLineProcessor implements LineProcessor<List<MappedRead>> {
-	List<MappedRead> mappedReadList = new ArrayList<>();
+	private List<MappedRead> mappedReadList = new ArrayList<>();
+
 	@Override
 	public boolean processLine(String line) throws IOException {
 		if (line.startsWith("chr")){
 			return true;
-		}else if(!line.equals("")){
+		}else if(line.equals("")){
+			return false;
+		}else {
 			List<String> itemList = Lists.newArrayList(Splitter.on('\t').split(line));
 			mappedReadList.add(new MappedRead(itemList.get(0), itemList.get(1), Long.parseLong(itemList.get(2)), Long.parseLong(itemList.get(3)), itemList.get(4),
 					Long.parseLong(itemList.get(5))));
 			return true;
-		}else {
-			return false;
 		}
 	}
 
