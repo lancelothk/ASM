3a402f7666c71f623f9294cc5dab3c080fa0c9c4
Ke Hu <me@huke.info>
2014-04-23 12:51:51 -0400

    add ASM detection

---
+++ ASM/src/ASM/DataType/Node.java
@@ -0,0 +1,16 @@
+package ASM.DataType;
+
+/**
+ * Created by ke on 4/23/14.
+ */
+public class Node {
+    private int index;
+
+    public Node(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
--- ASM/src/ASM/Execution/FindASM.java
+++ ASM/src/ASM/Execution/FindASM.java
@@ -3,6 +3,7 @@ package ASM.Execution;
 import ASM.DataType.CpGSite;
 import ASM.DataType.MappedRead;
 import ASM.DataType.MappedReadComparaterByCpG;
+import ASM.DataType.Node;
 import ASM.Utils.MappedReadFileLineProcessor;
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
@@ -19,9 +20,15 @@ import java.util.List;
  * Created by ke on 2/19/14.
  */
 public class FindASM {
+    private enum Compatibility {
+        COMPATIBLE, NONCOMPATIBLE, NONCONNECTED, SELF
+    }
+
+    private Compatibility[][] matrix;
+
     public static void main(String[] args) throws IOException {
         FindASM findASM = new FindASM();
-        findASM.execute("/home/ke/ASM_result/chr20/chr20-5727178-5728657.reads", 5727178);
+        findASM.execute("/home/ke/ASM_result/chr20/chr20-56861267-56862291.reads", 56861267);
     }
 
     public void execute(String intervalFileName, long initPos) throws IOException {
@@ -36,17 +43,74 @@ public class FindASM {
             }
         }
         Collections.sort(mappedReadList, new MappedReadComparaterByCpG());
-        findASM(mappedReadList);
+        constructGraph(mappedReadList);
+        detectASM(mappedReadList);
     }
 
-    private void findASM(List<MappedRead> mappedReadList) {
-        int[][] matrix = new int[mappedReadList.size()][mappedReadList.size()];
+    private List<List<Node>> detectASM(List<MappedRead> mappedReadList) {
+        List<List<Node>> resultList = new ArrayList<>();
+        List<Node> nodeList = new ArrayList<>();
+        for (int i = 0; i < mappedReadList.size(); i++) {
+            nodeList.add(new Node(i));
+        }
+        while (true) {
+            List<Node> result = getGroup(nodeList);
+            if (result.size() == 0) {
+                break;
+            } else {
+                resultList.add(result);
+                nodeList.removeAll(result);
+            }
+        }
+        return resultList;
+    }
+
+    private List<Node> getGroup(List<Node> nodeList) {
+        List<Node> result = new ArrayList<>();
+        List<Node> seedList = new ArrayList<>();
+        seedList.add(nodeList.get(0));
+        result.add(nodeList.get(0));
+        while (true) {
+            if (seedList.size() == 0) {
+                break;
+            } else {
+                addCandidates(nodeList, result, seedList, seedList.get(0));
+            }
+        }
+        return result;
+    }
+
+    private void addCandidates(List<Node> nodeList, List<Node> result, List<Node> seedList, Node seed) {
+        // check all other reads
+        for (int i = 0; i < matrix[seed.getIndex()].length; i++) {
+            if (matrix[seed.getIndex()][i] == Compatibility.COMPATIBLE) {
+                //if candidate has no contradiction with result member, add to result
+                if (!hasContradiction(result, i)) {
+                    seedList.add(nodeList.get(i));
+                    result.add(nodeList.get(i));
+                }
+            }
+        }
+        seedList.remove(seed);
+    }
+
+    private boolean hasContradiction(List<Node> result, int candidate) {
+        for (Node member : result) {
+            if (matrix[candidate][member.getIndex()] == Compatibility.NONCOMPATIBLE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void constructGraph(List<MappedRead> mappedReadList) {
+        matrix = new Compatibility[mappedReadList.size()][mappedReadList.size()];
         for (int i = 0; i < mappedReadList.size(); i++) {
             for (int j = 0; j < mappedReadList.size(); j++) {
                 if (i == j) {
-                    matrix[i][j] = 0;
+                    matrix[i][j] = Compatibility.SELF;
                 }else {
-                    matrix[i][j] =  comparable(mappedReadList.get(i), mappedReadList.get(j));
+                    matrix[i][j] = checkCompatible(mappedReadList.get(i), mappedReadList.get(j));
                 }
             }
         }
@@ -59,20 +123,20 @@ public class FindASM {
         }
     }
 
-    private int comparable(MappedRead readA, MappedRead readB) {
+    private Compatibility checkCompatible(MappedRead readA, MappedRead readB) {
         if (readA.getFirstCpG().getPos() <= readB.getLastCpG().getPos() &&
                 readA.getLastCpG().getPos() >= readB.getFirstCpG().getPos()) {
             for (CpGSite cpgA : readA.getCpgList()) {
                 for (CpGSite cpgB : readB.getCpgList()) {
                     if (cpgA.getPos() == cpgB.getPos() && cpgA.isMethylated() != cpgB.isMethylated()) {
-                        return 2;
+                        return Compatibility.NONCOMPATIBLE;
                     }
                 }
             }
-            return 1;
+            return Compatibility.COMPATIBLE;
         } else {
             // don't have overlapped CpG, ignore
-            return 3;
+            return Compatibility.NONCONNECTED;
         }
     }
 
