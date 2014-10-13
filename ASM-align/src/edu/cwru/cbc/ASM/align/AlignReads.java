db4a7b766f6e6802eaac24b40bec0246528b86ec
Ke Hu <me@huke.info>
2014-10-13 17:32:56 -0400

    fix guava dependency.

--- ASM-align/src/edu/cwru/cbc/ASM/align/AlignReads.java
+++ ASM-align/src/edu/cwru/cbc/ASM/align/AlignReads.java
@@ -8,80 +8,85 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Align reads to readable format with padding.
+ */
+
 public class AlignReads {
     private static String ref;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         String targetFileName;
         if (args.length == 0) {
             targetFileName = "/home/lancelothk/chr11-2022459-2056084.reads";
-        }else {
+        } else {
             targetFileName = args[0];
         }
-		File targetFile= new File(targetFileName);
-		if (targetFile.isDirectory()){
-			File[] files = targetFile.listFiles();
-			assert  files != null;
-			for (File file : files) {
-				try {
-					if (file.isFile() && !file.isHidden() && !file.getName().endsWith(".aligned")) {
-						String outputFileName = file.getAbsolutePath() + ".aligned";
-						List<Read> readsList = readMappedReads(file);
-						alignReads(readsList, outputFileName);
-					}
-				} catch (Exception e){
-					throw new Exception(file.getAbsolutePath(),e);
-				}
-			}
-		}else {
-			String outputFileName = targetFileName + ".aligned";
-			List<Read> readsList = readMappedReads(targetFile);
-			alignReads(readsList, outputFileName);
-		}
-	}
+        File targetFile = new File(targetFileName);
+        if (targetFile.isDirectory()) {
+            File[] files = targetFile.listFiles();
+            assert files != null;
+            for (File file : files) {
+                try {
+                    if (file.isFile() && !file.isHidden() && !file.getName().endsWith(".aligned")) {
+                        String outputFileName = file.getAbsolutePath() + ".aligned";
+                        List<Read> readsList = readMappedReads(file);
+                        alignReads(readsList, outputFileName);
+                    }
+                } catch (Exception e) {
+                    throw new Exception(file.getAbsolutePath(), e);
+                }
+            }
+        } else {
+            String outputFileName = targetFileName + ".aligned";
+            List<Read> readsList = readMappedReads(targetFile);
+            alignReads(readsList, outputFileName);
+        }
+    }
 
-	public static ArrayList<Read> readMappedReads(File inputFile) {
-		ArrayList<Read> readsList = new ArrayList<>();
-		BufferedReader bufferedReader;
-		try {
-			bufferedReader = new BufferedReader(new FileReader(inputFile));
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
+    public static ArrayList<Read> readMappedReads(File inputFile) {
+        ArrayList<Read> readsList = new ArrayList<>();
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFile));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
                 if (line.startsWith("ref")) {
                     ref = line.split("\t")[1];
                     continue;
                 }
                 String[] items = line.split("\t");
-				if (!items[1].equals("+") && !items[1].equals("-")) {
-					System.err.println("invalid strand symbol!");
-				}
-				readsList.add(new Read(items[0], items[1].charAt(0), Long.parseLong(items[2]), Long.parseLong(items[3]), items[4], items[5]));
-			}
-			bufferedReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-		return readsList;
-	}
+                if (!items[1].equals("+") && !items[1].equals("-")) {
+                    System.err.println("invalid strand symbol!");
+                }
+                readsList.add(new Read(items[0], items[1].charAt(0), Long.parseLong(items[2]), Long.parseLong(items[3]),
+                                       items[4], items[5]));
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return readsList;
+    }
 
-	public static void alignReads(List<Read> readsList, String outputFileName) {
-		// sort reads first
-		Collections.sort(readsList, new ReadComparator());
-		// set initial position
-		long initialPos = readsList.get(0).getStart();
-		BufferedWriter bufferedWriter;
-		try {
-			bufferedWriter = new BufferedWriter(new FileWriter(outputFileName));
+    public static void alignReads(List<Read> readsList, String outputFileName) {
+        // sort reads first
+        Collections.sort(readsList, new ReadComparator());
+        // set initial position
+        long initialPos = readsList.get(0).getStart();
+        BufferedWriter bufferedWriter;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(outputFileName));
             if (ref != null) {
                 bufferedWriter.write(String.format("ref:\t%s\n", ref));
             }
             for (Read read : readsList) {
-				bufferedWriter.write(read.toString(initialPos) + "\n");
-			}
-			bufferedWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+                bufferedWriter.write(read.toString(initialPos) + "\n");
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
