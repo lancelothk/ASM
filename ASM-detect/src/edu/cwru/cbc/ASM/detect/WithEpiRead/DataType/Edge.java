65f43964530278f9f5361fd05b7959cd69618acb
lancelothk <lancelothk@gmail.com>
2014-11-13 22:57:02 -0500

    implement EpiRead method

--- ASM-detect/src/edu/cwru/cbc/ASM/detect/WithEpiRead/DataType/Edge.java
+++ ASM-detect/src/edu/cwru/cbc/ASM/detect/WithEpiRead/DataType/Edge.java
@@ -4,12 +4,12 @@ package edu.cwru.cbc.ASM.detect.WithEpiRead.DataType;
  * Created by kehu on 11/13/14.
  * Edge for Matrxi represented graph
  */
-public class Edge {
+public class Edge implements Comparable<Edge> {
     private int left;
     private int right;
-    private int weight;
+    private double weight;
 
-    public Edge(int left, int right, int weight) {
+    public Edge(int left, int right, double weight) {
         this.left = left;
         this.right = right;
         this.weight = weight;
@@ -23,7 +23,13 @@ public class Edge {
         return right;
     }
 
-    public int getWeight() {
+    public double getWeight() {
         return weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        // larger first
+        return Double.compare(o.getWeight(), this.weight);
     }
 }
--- ASM-detect/src/edu/cwru/cbc/ASM/detect/WithEpiRead/DetectionWithEpiRead.java
+++ ASM-detect/src/edu/cwru/cbc/ASM/detect/WithEpiRead/DetectionWithEpiRead.java
@@ -5,13 +5,16 @@ import edu.cwru.cbc.ASM.commons.Utils;
 import edu.cwru.cbc.ASM.detect.Detection;
 import edu.cwru.cbc.ASM.detect.WithEpiRead.DataType.Edge;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by kehu on 11/12/14.
@@ -20,21 +23,52 @@ import java.util.Map;
 public class DetectionWithEpiRead extends Detection {
     private static final int MIN_READ_CPG = 2;
     private File intervalFile;
-    private BufferedWriter groupWriter;
     private EpiRead.EpiReadFormat format;
 
-    public DetectionWithEpiRead(File intervalFile, BufferedWriter summaryWriter, EpiRead.EpiReadFormat format) {
+    public DetectionWithEpiRead(File intervalFile, EpiRead.EpiReadFormat format) {
         this.intervalFile = intervalFile;
-        this.groupWriter = summaryWriter;
         this.format = format;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        String pathName = "/home/lancelothk/experiment/ASM/epiread/intervals_epiread/chr22-14476217-14476662";
+        String pathName = "/home/lancelothk/IdeaProjects/ASM/ASM-detect/testData/epiTest";
+        File path = new File(pathName);
+
+        EpiRead.EpiReadFormat format = EpiRead.EpiReadFormat.extEpiread;
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        List<Future<String>> futureList = new ArrayList<>();
+        if (path.isDirectory()) {
+            for (File file : path.listFiles()) {
+                if (file.isFile() && file.getName().startsWith("chr") && !file.getName().endsWith("aligned") &&
+                        !file.getName().endsWith("intervalSummary") && !file.getName().endsWith("~")) {
+                    String[] items = file.getName().split("-");
+                    Future<String> future = executor.submit(new DetectionWithEpiRead(file, format));
+                    futureList.add(future);
+                }
+            }
+        } else {
+            String[] items = path.getName().split("-");
+            Future<String> future = executor.submit(new DetectionWithEpiRead(path, format));
+            futureList.add(future);
+        }
+
+        for (Future<String> stringFuture : futureList) {
+            stringFuture.get();
+        }
+        executor.shutdown();
     }
 
     private double[][] buildInputMatrix() throws IOException {
         List<EpiRead> epireadList = Utils.readEpiReadFile(intervalFile, format);
         Map<Integer, Integer> cpgMap = new HashMap<>();
         epireadList.forEach(epiRead -> {
-            if (!cpgMap.containsKey(epiRead.getCpgOrder())) {
-                cpgMap.put(epiRead.getCpgOrder(), epiRead.getCpgPos());
+            int order = epiRead.getCpgOrder();
+            for (int i = 0; i < epiRead.getCpgSeq().length(); i++) {
+                if (!cpgMap.containsKey(order + i)) {
+                    cpgMap.put(order + i, 0);
+                }
             }
         });
         List<Integer> cpgs = new ArrayList<>(cpgMap.keySet());
@@ -74,25 +108,99 @@ public class DetectionWithEpiRead extends Detection {
 
     @Override
     public String call() throws Exception {
+        Map<Integer, Integer> p = new HashMap<>();
+        Map<Integer, Integer> partA = new HashMap<>();
+        Map<Integer, Integer> partB = new HashMap<>();
         double[][] inputMatrix = buildInputMatrix();
 //        double[][] conflictMatrix = buildConflictMatrix(inputMatrix);
         List<Edge> edgeList = new ArrayList<>();
         for (int i = 0; i < inputMatrix.length; i++) {
             for (int j = i + 1; j < inputMatrix.length; j++) {
-                int score = 0, coveredCount = 0;
+                double score = 0, coveredCount = 0;
                 for (int k = 0; k < inputMatrix[0].length; k++) {
-                    if (inputMatrix[i][k] == inputMatrix[j][k]) {
-
-                    } else if (inputMatrix[i][k] != inputMatrix[j][k] && inputMatrix[i][k] != 0 &&
-                            inputMatrix[j][k] != 0) {
-                        edgeList.add(new Edge(i, j, 1));
-                    } else {
-                        conflictMatrix[i][j] = 0.5;
+                    if (inputMatrix[i][k] != 0 || inputMatrix[j][k] != 0) {
+                        coveredCount++;
+                    }
+                    if (inputMatrix[i][k] != inputMatrix[j][k]) {
+                        if (inputMatrix[i][k] != 0 && inputMatrix[j][k] != 0) {
+                            score++;
+                        } else {
+                            score += 0.5;
+                        }
                     }
                 }
-                edgeList.add(new Edge(i, j, 0));
+                edgeList.add(new Edge(i, j, score / coveredCount));
             }
         }
+        edgeList.sort(Edge::compareTo);
+        int tieCount = 0;
+        if (edgeList.size() >= 2 && edgeList.get(0).getWeight() == edgeList.get(1).getWeight()) {
+            tieCount++;
+        }
+        // initialize partition
+        partA.put(edgeList.get(0).getLeft(), 0);
+        partB.put(edgeList.get(0).getRight(), 0);
+        p.put(edgeList.get(0).getLeft(), 0);
+        p.put(edgeList.get(0).getRight(), 0);
+        edgeList.remove(0);
+
+        while (edgeList.size() != 0) {
+            for (int i = 0; i < edgeList.size(); i++) {
+                Edge e = edgeList.get(i);
+                if (partA.containsKey(e.getLeft()) || partA.containsKey(e.getRight()) ||
+                        partB.containsKey(e.getLeft()) || partB.containsKey(e.getRight())) {
+                    if (edgeList.size() >= 2 && edgeList.get(0).getWeight() == edgeList.get(1).getWeight()) {
+                        tieCount++;
+                    }
+                    if (partA.containsKey(e.getLeft()) && !p.containsKey(e.getRight())) {
+                        partB.put(e.getRight(), 0);
+                        p.put(e.getRight(), 0);
+                    } else if (partA.containsKey(e.getRight()) && !p.containsKey(e.getLeft())) {
+                        partB.put(e.getLeft(), 0);
+                        p.put(e.getLeft(), 0);
+                    } else if (partB.containsKey(e.getLeft()) && !p.containsKey(e.getRight())) {
+                        partA.put(e.getRight(), 0);
+                        p.put(e.getRight(), 0);
+                    } else if (partB.containsKey(e.getRight()) && !p.containsKey(e.getLeft())) {
+                        partA.put(e.getLeft(), 0);
+                        p.put(e.getLeft(), 0);
+                    }
+                    edgeList.remove(i);
+                    break;
+                }
+            }
+            for (int i = edgeList.size() - 1; i > 0; i--) {
+                Edge e = edgeList.get(i);
+                if (partA.containsKey(e.getLeft()) || partA.containsKey(e.getRight()) ||
+                        partB.containsKey(e.getLeft()) || partB.containsKey(e.getRight())) {
+                    if (edgeList.size() >= 2 && edgeList.get(0).getWeight() == edgeList.get(1).getWeight()) {
+                        tieCount++;
+                    }
+                    if (partA.containsKey(e.getLeft()) && !p.containsKey(e.getRight())) {
+                        partA.put(e.getRight(), 0);
+                        p.put(e.getRight(), 0);
+                    } else if (partA.containsKey(e.getRight()) && !p.containsKey(e.getLeft())) {
+                        partA.put(e.getLeft(), 0);
+                        p.put(e.getLeft(), 0);
+                    } else if (partB.containsKey(e.getLeft()) && !p.containsKey(e.getRight())) {
+                        partB.put(e.getRight(), 0);
+                        p.put(e.getRight(), 0);
+                    } else if (partB.containsKey(e.getRight()) && !p.containsKey(e.getLeft())) {
+                        partB.put(e.getLeft(), 0);
+                        p.put(e.getLeft(), 0);
+                    }
+                    edgeList.remove(i);
+                    break;
+                }
+            }
+        }
+        System.out.println("PartA");
+        partA.forEach((i, j) -> System.out.printf("%d\t", i));
+        System.out.println();
+
+        System.out.println("PartB");
+        partB.forEach((i, j) -> System.out.printf("%d\t", i));
+        System.out.println();
         return "";
     }
 }
