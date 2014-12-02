c54f57ada8024b6dc995d69d1699e4fcc9bd5228
lancelothk <lancelothk@gmail.com>
2014-12-02 02:37:41 -0500

    added code to force merging clusters to 2 clusters

--- ASM-detect/src/edu/cwru/cbc/ASM/detect/WithMappedRead/DataType/Vertex.java
+++ ASM-detect/src/edu/cwru/cbc/ASM/detect/WithMappedRead/DataType/Vertex.java
@@ -85,15 +85,13 @@ public class Vertex {
 		double mec = 0;
 		for (MappedRead mappedRead : mappedReadList) {
 			for (CpG cpG : mappedRead.getCpgList()) {
-				// exclude N case first
-				if (cpG.getMethylStatus() != MethylStatus.N) {
 					MethylStatus refMethylStatus = refCpGMap.get(cpG.getPos()).getMajorMethylStatus();
+				// TODO double check the N in calculation of MEC
 					if (refMethylStatus == MethylStatus.N) {
 						mec += 0.5;
-					} else if (cpG.getMethylStatus() != refMethylStatus) {
+					} else if (cpG.getMethylStatus() != refMethylStatus && cpG.getMethylStatus() != MethylStatus.N) {
 						mec++;
 					}
-				}
 			}
 		}
 		return mec;
