eb24c9aeb171adefa0806ba6ac66dd1c8a575384
Ke Hu <me@huke.info>
2015-01-29 15:12:38 -0500

    update read stat summary.

--- ASM-commons/src/edu/cwru/cbc/ASM/commons/DataType/MappedReadLineProcessorWithSummary.java
+++ ASM-commons/src/edu/cwru/cbc/ASM/commons/DataType/MappedReadLineProcessorWithSummary.java
@@ -11,13 +11,18 @@ import java.util.logging.Logger;
  */
 public class MappedReadLineProcessorWithSummary extends MappedReadLineProcessor {
     private static final Logger logger = Logger.getLogger(MappedReadLineProcessorWithSummary.class.getName());
-    private long totalLength;
+    private long totalLength, qualifiedTotalLength;
     private long count;
     private int maxLength = Integer.MIN_VALUE;
     private int minLength = Integer.MAX_VALUE;
+    private int qualifiedMaxLength = Integer.MIN_VALUE;
+    private int qualifiedMinLength = Integer.MAX_VALUE;
     private long totalCpGCount;
+    private long qualifiedTotalCpGCount;
     private int maxCpGCount = Integer.MIN_VALUE;
     private int minCpGCount = Integer.MAX_VALUE;
+    private int qualifiedMaxCpGCount = Integer.MIN_VALUE;
+    private int qualifiedMinCpGCount = Integer.MAX_VALUE;
     private BitSet chrBitSet;
 
     public MappedReadLineProcessorWithSummary(List<RefCpG> refCpGList, int refLength) throws IOException {
@@ -33,6 +38,14 @@ public class MappedReadLineProcessorWithSummary extends MappedReadLineProcessor 
             int cpgCount = mappedRead.getCpgList().size();
 
             //stat variables
+            if (cpgCount >= MappedReadLineProcessor.MIN_READ_CPG) {
+                qualifiedTotalLength += length;
+                qualifiedMaxLength = length > qualifiedMaxLength ? length : qualifiedMaxLength;
+                qualifiedMinLength = length < qualifiedMinLength ? length : qualifiedMinLength;
+                qualifiedTotalCpGCount += cpgCount;
+                qualifiedMaxCpGCount = cpgCount > qualifiedMaxCpGCount ? cpgCount : qualifiedMaxCpGCount;
+                qualifiedMinCpGCount = cpgCount < qualifiedMinCpGCount ? cpgCount : qualifiedMinCpGCount;
+            }
             count++;
             totalLength += length;
             maxLength = length > maxLength ? length : maxLength;
@@ -45,7 +58,6 @@ public class MappedReadLineProcessorWithSummary extends MappedReadLineProcessor 
         } catch (Exception e) {
             throw new RuntimeException("Problem line: " + s + "\n", e);
         }
-
     }
 
     @Override
@@ -64,8 +76,14 @@ public class MappedReadLineProcessorWithSummary extends MappedReadLineProcessor 
         sb.append(String.format("totalReadCount:%d\tQualifiedReadCount:%d\n", count, mappedReadList.size()));
         sb.append(String.format("totalLength:%d\tavgLength:%f\tmaxLength:%d\tminLength:%d\n", totalLength,
                                 totalLength / (double) count, maxLength, minLength));
+        sb.append(String.format("qualified: totalLength:%d\tavgLength:%f\tmaxLength:%d\tminLength:%d\n",
+                                qualifiedTotalLength, qualifiedTotalLength / (double) mappedReadList.size(),
+                                qualifiedMaxLength, qualifiedMinLength));
         sb.append(String.format("totalCpGCount:%d\tavgCpgCount:%f\tmaxCpgCount:%d\tminCpgCount:%d\n", totalCpGCount,
                                 totalCpGCount / (double) count, maxCpGCount, minCpGCount));
+        sb.append(String.format("qualified: totalCpGCount:%d\tavgCpgCount:%f\tmaxCpgCount:%d\tminCpgCount:%d\n",
+                                qualifiedTotalCpGCount, qualifiedTotalCpGCount / (double) mappedReadList.size(),
+                                qualifiedMaxCpGCount, qualifiedMinCpGCount));
         sb.append(String.format("refLength:%d\trefCpgSiteNumber:%d\n", chrBitSet.size(), refMap.size()));
         sb.append(String.format("readCoverage:%f\tcoveredLength:%d\n", totalLength / (double) chrBitSet.size(),
                                 chrBitSet.cardinality()));
@@ -81,7 +99,6 @@ public class MappedReadLineProcessorWithSummary extends MappedReadLineProcessor 
         sb.append(String.format(
                 "totalCpGCountInRefMap:%d\tavg cpgSite coverage:%f\tmaxCpGCoverage:%d\tminCpGCoverage:%d\n",
                 totalCpGCountInRefMap, totalCpGCountInRefMap / (double) refMap.size(), maxCpGCoverage, minCpGCoverage));
-
         logger.warning(sb.toString());
     }
 }
