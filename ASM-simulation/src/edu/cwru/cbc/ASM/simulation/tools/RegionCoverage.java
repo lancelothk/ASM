46f3587d6ab8ba80cb661a411b372d973551cfb9
Ke Hu <me@huke.info>
2015-03-02 17:12:46 -0500

    start working on regeneration of target regions

--- ASM-simulation/src/edu/cwru/cbc/ASM/simulation/tools/RegionCoverage.java
+++ ASM-simulation/src/edu/cwru/cbc/ASM/simulation/tools/RegionCoverage.java
@@ -4,67 +4,76 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.BitSet;
 
 /**
  * Created by lancelothk on 2/24/15.
  * Get the coverage details of a genomic region.
  */
 public class RegionCoverage {
-	public static void main(String[] args) throws IOException {
-		String currUserHome = System.getProperty("user.home");
-		String inputFolderName = currUserHome + "/experiments/ASM/simulation/CpGIslandsRegions/";
-		String prefix = "i90_r1_chr20_";
+    private static final int COVERAGE_THRESHOLD = 8;
 
-		File inputFolder = new File(inputFolderName);
-		if (!inputFolder.isDirectory()){
-			throw new RuntimeException("input is not a folder!");
-		}
+    public static void main(String[] args) throws IOException {
+        String currUserHome = System.getProperty("user.home");
+        String inputFolderName = currUserHome + "/experiments/ASM/simulation/CpGIslandsRegions/";
+        String prefix = "i90_r1_chr20_";
 
-		File[] inputFiles = inputFolder.listFiles();
-		for (File inputFile : inputFiles) {
-			if (inputFile.getName().startsWith(prefix) && !inputFile.getName().endsWith(".aligned")){
-				printQualifiedLength(inputFile, prefix);
-			}
-		}
+        File inputFolder = new File(inputFolderName);
+        if (!inputFolder.isDirectory()) {
+            throw new RuntimeException("input is not a folder!");
+        }
 
-
-	}
-
-	private static void printQualifiedLength(File inputFile, String prefix) throws IOException {
-		String[] items = inputFile.getName().replace(prefix, "").split("-");
-		if (items.length != 2){
-			throw new RuntimeException("invalid file name format!");
-		}
-
-		int regionStart = Integer.valueOf(items[0]);
-		int regionEnd = Integer.valueOf(items[1]);
+        File[] inputFiles = inputFolder.listFiles();
+        for (File inputFile : inputFiles) {
+            if (inputFile.getName().startsWith(prefix) && !inputFile.getName().endsWith(".aligned")) {
+                selectHighCoverageRegion(inputFile, prefix);
+            }
+        }
 
 
-		int[] coverage = new int[regionEnd - regionStart + 1];
+    }
 
-		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-		String line;
-		while ((line = reader.readLine()) != null) {
-			items = line.split("\t");
-			int start = Integer.valueOf(items[2]);
-			int end = Integer.valueOf(items[3]);
-			for (int i = start; i <= end; i++) {
-				if (i >= regionStart && i <= regionEnd) {
-					coverage[i - regionStart]++;
-				}
-			}
-		}
+    private static void selectHighCoverageRegion(File inputFile, String prefix) throws IOException {
+        String[] items = inputFile.getName().replace(prefix, "").split("-");
+        if (items.length != 2) {
+            throw new RuntimeException("invalid file name format!");
+        }
 
-		int count = 0;
-		for (int i : coverage) {
-			//System.out.print(i + "\t");
-            if (i >= 8) {
-                count++;
-			}
-		}
-		//System.out.println();
-		System.out.println(inputFile.getName() + "\t" + count);
+        int regionStart = Integer.valueOf(items[0]);
+        int regionEnd = Integer.valueOf(items[1]);
 
-		reader.close();
-	}
+
+        int[] coverage = new int[regionEnd - regionStart + 1];
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            items = line.split("\t");
+            int start = Integer.valueOf(items[2]);
+            int end = Integer.valueOf(items[3]);
+            for (int i = start; i <= end; i++) {
+                if (i >= regionStart && i <= regionEnd) {
+                    coverage[i - regionStart]++;
+                }
+            }
+        }
+
+        BitSet bitSet = new BitSet(regionEnd - regionStart + 1);
+        for (int i = 0; i < coverage.length; i++) {
+            if (coverage[i] >= COVERAGE_THRESHOLD) {
+                bitSet.set(i);
+            }
+        }
+
+        int nextTrue = -1;
+        int nextFalse = -1;
+        while ((nextTrue = bitSet.nextSetBit(nextFalse + 1)) != -1) {
+            if ((nextFalse = bitSet.nextClearBit(nextTrue)) != -1) {
+                System.out.printf("region:%d-%d\n", nextTrue, nextFalse - 1);
+            } else {
+                break;
+            }
+        }
+        reader.close();
+    }
 }
