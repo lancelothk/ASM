c75211c4cf90ea145b15a9803f5784b56fe05612
Ke Hu <me@huke.info>
2015-05-28 16:24:58 -0400

    make output of intersection more clear.

--- ASM-intersect/src/edu/cwru/cbc/ASM/intersect/IntersectRegionsPgm.java
+++ ASM-intersect/src/edu/cwru/cbc/ASM/intersect/IntersectRegionsPgm.java
@@ -1,5 +1,6 @@
 package edu.cwru.cbc.ASM.intersect;
 
+import com.google.common.base.Strings;
 import edu.cwru.cbc.ASM.commons.GenomicInterval;
 import edu.cwru.cbc.ASM.commons.bed.BedUtils;
 import org.apache.commons.cli.*;
@@ -17,51 +18,60 @@ public class IntersectRegionsPgm {
 		Options options = new Options();
 		options.addOption("a", true, "bed file A");
 		options.addOption("b", true, "bed file B");
-        options.addOption("o", true, "output file name");
+		options.addOption("o", true, "output file name");
 
 		CommandLineParser parser = new BasicParser();
 		CommandLine cmd = parser.parse(options, args);
 
 		String aFileName = cmd.getOptionValue("a");
 		String bFileName = cmd.getOptionValue("b");
-        String outputFileName = cmd.getOptionValue("o");
+		String outputFileName = cmd.getOptionValue("o");
 
-        execution(aFileName, bFileName, outputFileName);
-    }
+		execution(aFileName, bFileName, outputFileName);
+	}
 
-    private static void execution(String aFileName, String bFileName, String outputFileName) throws IOException {
-        Collection<GenomicInterval> regionsA = BedUtils.readSingleChromBedRegions(aFileName, true);
+	private static void execution(String aFileName, String bFileName, String outputFileName) throws IOException {
+		Collection<GenomicInterval> regionsA = BedUtils.readSingleChromBedRegions(aFileName, true);
 		Collection<GenomicInterval> regionsB = BedUtils.readSingleChromBedRegions(bFileName, true);
 		Collection<GenomicInterval> intersections = BedUtils.intersect(regionsA, regionsB);
-        BedUtils.writeBedRegions(intersections, outputFileName);
-        BedUtils.writeBedWithIntersection(regionsA,
-                new File(aFileName).getAbsolutePath() + "_intersected.bed");
-        BedUtils.writeBedWithIntersection(regionsB,
-                new File(bFileName).getAbsolutePath() + "_intersected.bed");
+		BedUtils.writeBedRegions(intersections, outputFileName);
+		BedUtils.writeBedWithIntersection(regionsA,
+				new File(aFileName).getAbsolutePath() + "_intersected.bed");
+		BedUtils.writeBedWithIntersection(regionsB,
+				new File(bFileName).getAbsolutePath() + "_intersected.bed");
 
-		double intersectedCount_regionA = regionsA.stream().filter(GenomicInterval::isIntersected).count();
-		double nonIntersectedCount_regionA = regionsA.stream().filter(r -> !r.isIntersected()).count();
-		double intersectedCount_regionB = regionsB.stream().filter(GenomicInterval::isIntersected).count();
-		double nonIntersectedCount_regionB = regionsB.stream().filter(r -> !r.isIntersected()).count();
+		long intersectedCount_regionA = regionsA.stream().filter(GenomicInterval::isIntersected).count();
+		long nonIntersectedCount_regionA = regionsA.stream().filter(r -> !r.isIntersected()).count();
+		long intersectedCount_regionB = regionsB.stream().filter(GenomicInterval::isIntersected).count();
+		long nonIntersectedCount_regionB = regionsB.stream().filter(r -> !r.isIntersected()).count();
 		int intersectionLength = intersections.stream().mapToInt(GenomicInterval::length).sum();
-		int nonIntersectionLength_regionA =
-				regionsA.stream().mapToInt(GenomicInterval::length).sum() - intersectionLength;
-		int nonIntersectionLength_regionB =
-				regionsB.stream().mapToInt(GenomicInterval::length).sum() - intersectionLength;
+		int totalLengthRegionA = regionsA.stream().mapToInt(GenomicInterval::length).sum();
+		int totalLengthRegionB = regionsB.stream().mapToInt(GenomicInterval::length).sum();
 
+		System.out.println(Strings.padStart("", 80, '*'));
+		System.out.printf("A is %s\n", aFileName);
+		System.out.printf("B is %s\n", bFileName);
+
+		System.out.println(Strings.padStart("", 80, '*'));
 		System.out.printf("#intersection:%d\n", intersections.size());
-		System.out.printf("non-intersected length of A:%d\n", nonIntersectionLength_regionA);
-		System.out.printf("non-intersected length of B:%d\n", nonIntersectionLength_regionB);
 		System.out.printf("total length of intersection:%d\n", intersectionLength);
-		System.out.printf("#covered regions in A:%.4f(%.4f)\n", intersectedCount_regionA,
-				intersectedCount_regionA / regionsA.size());
-		System.out.printf("#not covered regions in A:%.4f(%.4f)\n", nonIntersectedCount_regionA,
-				nonIntersectedCount_regionA / regionsA.size());
-		System.out.printf("#regions in A:%d\n", regionsA.size());
-		System.out.printf("#covered regions in B:%.4f(%.4f)\n", intersectedCount_regionB,
-				intersectedCount_regionB / regionsB.size());
-		System.out.printf("#not covered regions in B:%.4f(%.4f)\n", nonIntersectedCount_regionB,
-				nonIntersectedCount_regionB / regionsB.size());
-		System.out.printf("#regions in B:%d\n", regionsB.size());
+
+		System.out.println(Strings.padStart("", 80, '*'));
+		System.out.printf("total #regions in A:%d\n", regionsA.size());
+		System.out.printf("total length of A:%d\n", totalLengthRegionA);
+		System.out.printf("#covered regions in A:%d(%.4f)\n", intersectedCount_regionA,
+				(double) intersectedCount_regionA / regionsA.size());
+		System.out.printf("percent of intersected length of A:%.4f\n", (double) intersectionLength / totalLengthRegionA);
+		assert intersectedCount_regionA + nonIntersectedCount_regionA == regionsA.size();
+
+		System.out.println(Strings.padStart("", 80, '*'));
+		System.out.printf("total #regions in B:%d\n", regionsB.size());
+		System.out.printf("total length of B:%d\n", totalLengthRegionB);
+		System.out.printf("#covered regions in B:%d(%.4f)\n", intersectedCount_regionB,
+				(double) intersectedCount_regionB / regionsB.size());
+		System.out.printf("percent of intersected length of B:%.4f\n", (double) intersectionLength / totalLengthRegionB);
+		assert intersectedCount_regionB + nonIntersectedCount_regionB == regionsB.size();
+
+		System.out.println(Strings.padStart("", 80, '*'));
 	}
 }
