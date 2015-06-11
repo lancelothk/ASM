386088e8d3f7d0f8f9a0760060d4d14df4f61536
lancelothk <lancelothk@gmail.com>
2015-06-10 21:43:19 -0400

    finished FASTALineProcessor and its unit tests.

---
+++ ASM-commons/src/edu/cwru/cbc/ASM/commons/IO/FASTALineProcessor.java
@@ -0,0 +1,90 @@
+package edu.cwru.cbc.ASM.commons.IO;
+
+import com.google.common.io.LineProcessor;
+import edu.cwru.cbc.ASM.commons.Sequence.FASTASequence;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.regex.Pattern;
+
+/**
+ * Created by lancelothk on 6/10/15.
+ * LineProcessor for reading FASTA format file.
+ */
+public class FASTALineProcessor implements LineProcessor<LinkedHashMap<String, FASTASequence>> {
+	// used to check if id is unique and also keep order of sequence as reading order.
+	private LinkedHashMap<String, FASTASequence> resultMap = new LinkedHashMap<>();
+	private int stringBuilderInitCapacity = -1; // -1 means use default setting.
+	private String id = null;
+	private StringBuilder sb;
+
+	public FASTALineProcessor() {
+		this.sb = new StringBuilder();
+	}
+
+	/**
+	 * For reading ultra-long sequence. E.g. reference sequence.
+	 */
+	public FASTALineProcessor(int stringBuilderInitCapacity) {
+		this.stringBuilderInitCapacity = stringBuilderInitCapacity;
+		// constructor of StringBuilder will check if the capacity value is valid. No need to do it here.
+		this.sb = new StringBuilder(stringBuilderInitCapacity);
+	}
+
+	/**
+	 * 1. Support both single/multi fasta format.
+	 * 2. When meet multiple sequence lines, combine them into one line.
+	 * 3. Sequence should not contain character other than acgtnACGTN and '.'.
+	 * Notice: this method shouldn't be used individually without calling {@link #getResult()} to finish the file
+	 * processing.
+	 */
+	@Override
+	public boolean processLine(String line) throws IOException {
+		if (line.startsWith(">")) {
+			if (id == null) { // first sequence
+				id = line.substring(1, line.length());
+			} else {
+				if (sb.length() == 0) {
+					throw new RuntimeException("missing sequence line!");
+				}
+				if (resultMap.containsKey(id)) {
+					throw new RuntimeException("duplicate id detected!");
+				} else if (sb.length() != 0) {
+					resultMap.put(id, new FASTASequence(id, sb.toString()));
+					if (stringBuilderInitCapacity == -1) {
+						sb = new StringBuilder();
+					} else {
+						sb = new StringBuilder(stringBuilderInitCapacity);
+					}
+				}
+				id = line.substring(1, line.length());
+			}
+		} else {
+			if (id == null) {
+				throw new RuntimeException("missing '>' in the beginning of file!");
+			}
+			if (Pattern.compile("[^ACGTNacgtn\\.]").matcher(line).find()) {
+				throw new RuntimeException("invalid character in sequence! only acgtnACGTN and '.' are allowed!");
+			}
+			sb.append(line);
+		}
+		return true;
+	}
+
+	@Override
+	public LinkedHashMap<String, FASTASequence> getResult() {
+		// put last FASTA sequence to map.
+		if (id != null) {
+			if (sb.length() == 0) {
+				throw new RuntimeException("missing sequence line!");
+			} else {
+				if (resultMap.containsKey(id)) {
+					throw new RuntimeException("duplicate id detected!");
+				} else {
+					resultMap.put(id, new FASTASequence(id, sb.toString()));
+				}
+			}
+		}
+		return resultMap;
+	}
+}
---
+++ ASM-commons/test/edu/cwru/cbc/ASM/commons/IO/FASTALineProcessorTest.java
@@ -0,0 +1,125 @@
+package edu.cwru.cbc.ASM.commons.IO;
+
+import edu.cwru.cbc.ASM.commons.Sequence.FASTASequence;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.LinkedHashMap;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by lancelothk on 6/10/15. Tests for FASTALineProcessor
+ */
+public class FASTALineProcessorTest {
+
+	@Test(expected = RuntimeException.class)
+	public void test_processLine_badStart() throws Exception {
+		String badStart = "AAAAA";
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(badStart);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void test_processLine_invalidCharacter() throws Exception {
+		String invalidCharacters = "QWERYUIOPSDFHJKLZXVBMqweryuiopsdfhjklzxvbm@#$%+_=-~!^&*()/\\?><,:;\"\' ";
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(">test");
+		for (char c : invalidCharacters.toCharArray()) {
+			flp.processLine("" + c);
+		}
+
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void test_processLine_duplicateID() throws Exception {
+		FASTALineProcessor flp = new FASTALineProcessor();
+		// Since the last sequence unit is put into map only when getResult called, processLine cannot detect duplicate
+		// in last seuqnce unit. So use 3 units here.
+		flp.processLine(">test");
+		flp.processLine("ACGTN");
+		flp.processLine(">test");
+		flp.processLine("acgtn");
+		flp.processLine(">test");
+		flp.processLine("ACGTN");
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void test_getResult_duplicateID() throws Exception {
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(">test");
+		flp.processLine("ACGTN");
+		flp.processLine(">test");
+		flp.processLine("acgtn");
+		flp.getResult();
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void test_processLine_missingSequence() throws Exception {
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(">test");
+		flp.processLine(">test");
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void test_getResult_MissingSequence() throws Exception {
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(">test");
+		flp.getResult();
+	}
+
+	@Test
+	public void test_processLine_validCharacter() throws Exception {
+		String validCharacter = "ACGTNacgtn.";
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(">test");
+		flp.processLine(validCharacter);
+		LinkedHashMap<String, FASTASequence> resultMap = flp.getResult();
+		assertEquals("incorrect sequence!", "ACGTNacgtn.", resultMap.get("test").getSequence());
+	}
+
+
+	@Test
+	public void test_processLine_multipleUnit() throws Exception {
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(">test1");
+		flp.processLine("ACGTN");
+		flp.processLine(">test2");
+		flp.processLine("acgtn");
+		LinkedHashMap<String, FASTASequence> resultMap = flp.getResult();
+		assertEquals("incorrect number of sequence!", 2, resultMap.size());
+		assertEquals("incorrect sequence!", "ACGTN", resultMap.get("test1").getSequence());
+		assertEquals("incorrect sequence!", "acgtn", resultMap.get("test2").getSequence());
+	}
+
+	@Test
+	public void test_processLine_multipleLineSequence() throws Exception {
+		FASTALineProcessor flp = new FASTALineProcessor();
+		flp.processLine(">test1");
+		flp.processLine("ACGTN");
+		flp.processLine("ACGTN");
+		flp.processLine("ACGTN");
+		flp.processLine(">test2");
+		flp.processLine("acgtn");
+		flp.processLine("acgtn");
+		flp.processLine("acgtn");
+		LinkedHashMap<String, FASTASequence> resultMap = flp.getResult();
+		assertEquals("incorrect number of sequence!", 2, resultMap.size());
+		assertEquals("incorrect sequence!", "ACGTNACGTNACGTN", resultMap.get("test1").getSequence());
+		assertEquals("incorrect sequence!", "acgtnacgtnacgtn", resultMap.get("test2").getSequence());
+	}
+
+	@Test
+	public void test_getResult_emptyInput() throws Exception {
+		FASTALineProcessor flp = new FASTALineProcessor();
+		LinkedHashMap<String, FASTASequence> resultMap = flp.getResult();
+		assertEquals("incorrect number of sequence!", 0, resultMap.size());
+	}
+
+	private <T> T getField(Object parent, Class<T> clazz,
+	                       String fieldName) throws NoSuchFieldException, IllegalAccessException {
+		Field f = parent.getClass().getDeclaredField(fieldName);
+		f.setAccessible(true);
+		return clazz.cast(f.get(parent));
+	}
+}
