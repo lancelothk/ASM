a81728b61e37b5fc904a471e62076dec251df206
Ke Hu <me@huke.info>
2015-06-11 17:58:07 -0400

    add one more unit test.

--- ASM-commons/test/edu/cwru/cbc/ASM/commons/Methylation/MethylationUtilsTest.java
+++ ASM-commons/test/edu/cwru/cbc/ASM/commons/Methylation/MethylationUtilsTest.java
@@ -13,6 +13,7 @@ public class MethylationUtilsTest {
 
 	@Test
 	public void testExtractCpGSite() throws Exception {
+		// covered cases: 1. CG in the beginning; 2.CG in the end; 3. CG in the middle.
 		List<RefCpG> refCpGList = extractCpGSite(
 				"CGCATTCCGATGCAGAATGTCCTTCATGAGAGGCGACTTTTTAGGACTTTTAATCTGCGTTCAAATCAATTAATAGTTTGACG", 17207804);
 
@@ -25,5 +26,15 @@ public class MethylationUtilsTest {
 		assertEquals("incorrect RefCpG position!", refCpGList.get(2).getPos(), 17207837);
 		assertEquals("incorrect RefCpG position!", refCpGList.get(3).getPos(), 17207861);
 		assertEquals("incorrect RefCpG position!", refCpGList.get(4).getPos(), 17207885);
+	}
+
+	@Test
+	public void test_ExtractCpGSite_shortRef() throws Exception {
+		List<RefCpG> refCpGList = extractCpGSite("CG", 17207804);
+		assertEquals("incorrect refCpGList size", refCpGList.size(), 1);
+		assertEquals("incorrect RefCpG position!", refCpGList.get(0).getPos(), 17207804);
+
+		refCpGList = extractCpGSite("G", 17207805);
+		assertEquals("incorrect refCpGList size", refCpGList.size(), 0);
 	}
 }
