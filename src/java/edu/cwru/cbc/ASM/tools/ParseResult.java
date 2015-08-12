9213dddbdd9ef59561b73653a5a2b715f0cbc746
Ke Hu <me@huke.info>
2015-08-12 11:38:43 -0400

    cmdlise ParseResult

--- src/java/edu/cwru/cbc/ASM/tools/ParseResult.java
+++ src/java/edu/cwru/cbc/ASM/tools/ParseResult.java
@@ -3,6 +3,7 @@ package edu.cwru.cbc.ASM.tools;
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 import com.google.common.io.LineProcessor;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,8 +13,13 @@ import java.io.IOException;
  * Used for parsing experiment intersection result.
  */
 public class ParseResult {
-	public static void main(String[] args) throws IOException {
-		String result = Files.readLines(new File("/home/lancelothk/jun14_filterReads_intersection"), Charsets.UTF_8,
+	public static void main(String[] args) throws IOException, ParseException {
+		Options options = new Options();
+		options.addOption(Option.builder("i").hasArg().desc("input result file").build());
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+		String resultFileName = cmd.getOptionValue("i");
+		String result = Files.readLines(new File(resultFileName), Charsets.UTF_8,
 				new LineProcessor<String>() {
 					private StringBuilder sb = new StringBuilder();
 
@@ -25,9 +31,12 @@ public class ParseResult {
 							sb.append(line).append("\n");
 						} else if (line.startsWith("0")) {
 							sb.append(line).append("\t");
-						} else if (line.startsWith("#covered regions in A")) {
-							sb.append(line.split(":")[1].split("\\(")[0]).append("\n");
+						} else if (line.startsWith("#covered regions in")) {
+							sb.append(line.split(":")[1].split("\\(")[0]).append("\t");
+							sb.append(line.split(":")[1].split("\\(")[1].replace(")", "")).append("\t");
 						} else if (line.startsWith("total length of intersection")) {
+							sb.append(line.split(":")[1]).append("\t");
+						} else if (line.startsWith("percent of intersected length of")) {
 							sb.append(line.split(":")[1]).append("\t");
 						}
 						return true;
@@ -39,6 +48,6 @@ public class ParseResult {
 					}
 
 				});
-		Files.write(result, new File("/home/lancelothk/filter"), Charsets.UTF_8);
+		System.out.println(result);
 	}
 }
