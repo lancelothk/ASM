5028a36b9106d344e371e9f5364de48c410998c4
Ke Hu <me@huke.info>
2015-11-11 13:28:46 -0500

    add output file for FDRControlPgm

--- src/java/edu/cwru/cbc/ASM/tools/FDRControlPgm.java
+++ src/java/edu/cwru/cbc/ASM/tools/FDRControlPgm.java
@@ -8,7 +8,9 @@ import edu.cwru.cbc.ASM.detect.FDRControl;
 import org.apache.commons.cli.*;
 
 import javax.annotation.Nonnull;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,17 +24,20 @@ public class FDRControlPgm {
 
 	public static void main(String[] args) throws ParseException, IOException {
 		Options options = new Options();
-		options.addOption(Option.builder("i").hasArg().desc("input grouped read file").build());
+		options.addOption(Option.builder("i").hasArg().desc("input summary file").build());
+		options.addOption(Option.builder("o").hasArg().desc("output file with region pass FDR control").build());
 		options.addOption(Option.builder("c").hasArg().desc("column of p values").build());
 		options.addOption(Option.builder("f").hasArg().desc("fdr threshold").build());
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd = parser.parse(options, args);
 		String inputFile = cmd.getOptionValue("i");
+		String outputFile = cmd.getOptionValue("o");
 		double fdr = Double.parseDouble(cmd.getOptionValue("f"));
 		int column = Integer.parseInt(cmd.getOptionValue("c")) - 1;
 
-		double correctedThreshold = Files.readLines(new File(inputFile), Charsets.UTF_8, new LineProcessor<Double>() {
+		Files.readLines(new File(inputFile), Charsets.UTF_8, new LineProcessor<Double>() {
 			private List<Double> pvalueList = new ArrayList<>();
+			private List<String> lines = new ArrayList<>();
 
 			@Override
 			public boolean processLine(@Nonnull String line) throws IOException {
@@ -41,16 +46,32 @@ public class FDRControlPgm {
 					return true;
 				} else {
 					pvalueList.add(Double.parseDouble(itemList.get(column)));
+					lines.add(line);
 					return true;
 				}
 			}
 
 			@Override
 			public Double getResult() {
-				return FDRControl.getBHYFDRCutoff(pvalueList, fdr);
+				double correctedThreshold = FDRControl.getBHYFDRCutoff(pvalueList, fdr);
+
+				try {
+					BufferedWriter outputWriter = new BufferedWriter(new FileWriter(outputFile));
+					for (int i = 0; i < lines.size(); i++) {
+						if (pvalueList.get(i) <= correctedThreshold) {
+							outputWriter.write(lines.get(i) + "\t");
+						}
+					}
+					outputWriter.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+
+				System.out.println("corrected threshold:" + correctedThreshold);
+				return null;
 			}
 		});
 
-		System.out.println("corrected threshold:" + correctedThreshold);
+
 	}
 }
