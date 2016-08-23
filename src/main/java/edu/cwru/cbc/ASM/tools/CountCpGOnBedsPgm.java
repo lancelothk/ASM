4302e6441169b607b9bca486acefcf0d73548f6c
Ke Hu <me@huke.info>
2016-08-23 15:45:12 -0400

    revise CountCpGOnBedsPgm.

--- src/main/java/edu/cwru/cbc/ASM/tools/CountCpGOnBedsPgm.java
+++ src/main/java/edu/cwru/cbc/ASM/tools/CountCpGOnBedsPgm.java
@@ -6,6 +6,7 @@ import edu.cwru.cbc.ASM.commons.io.IOUtils;
 import edu.cwru.cbc.ASM.commons.methylation.MethylationUtils;
 import edu.cwru.cbc.ASM.commons.methylation.RefCpG;
 import net.openhft.koloboke.collect.map.hash.HashIntObjMap;
+import org.apache.commons.cli.*;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -20,13 +21,28 @@ import java.util.stream.Collectors;
  * Count number of CpG sites on bed regions
  */
 public class CountCpGOnBedsPgm {
-	public static void main(String[] args) throws IOException {
-		String referenceFilePath = "/home/kehu/dataset/hg19";
-		String bedFileName = "/home/kehu/experiments/ASM/result/ASM/whole/union.bed.whole";
-		String intervalCpGCountFile = "/home/kehu/bedRegionWithCpGCount";
-		Map<String, List<BedInterval>> bedRegionMap = BedUtils.readBedRegions(bedFileName);
+	public static void main(String[] args) throws IOException, ParseException {
+		Options options = new Options();
+		options.addOption(Option.builder("r").hasArg().desc("Reference File Path").required().build());
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+
+		String referenceFilePath = cmd.getOptionValue("r");
+		long startTime = System.currentTimeMillis();
 		Map<String, HashIntObjMap<RefCpG>> genomeRefCpGMap = MethylationUtils.initializeGenomeRefCpGMap(
 				IOUtils.readReferenceGenome(referenceFilePath));
+		System.out.println("read reference finished: " + (System.currentTimeMillis() - startTime) / 1000);
+		for (String bedFileName : cmd.getArgList()) {
+			System.out.println(bedFileName);
+			bedFileCpGCount(genomeRefCpGMap, bedFileName);
+		}
+	}
+
+	private static void bedFileCpGCount(Map<String, HashIntObjMap<RefCpG>> genomeRefCpGMap, String bedFileName) throws
+			IOException {
+		String intervalCpGCountFile = bedFileName + ".CpGCount";
+		Map<String, List<BedInterval>> bedRegionMap = BedUtils.readBedRegions(bedFileName);
 		for (List<BedInterval> bedIntervals : bedRegionMap.values()) {
 			for (BedInterval bedInterval : bedIntervals) {
 				for (int i = bedInterval.getStart(); i <= bedInterval.getEnd(); i++) {
@@ -45,8 +61,15 @@ public class CountCpGOnBedsPgm {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(intervalCpGCountFile));
 		for (BedInterval bedInterval : allIntervalList) {
 			writer.write(String.format("%s\t%d\t%d\t%d\n", bedInterval.getChr(), bedInterval.getStart(),
-					bedInterval.getEnd(), bedInterval.getRefCpGList().size()));
+					bedInterval.getEnd() + 1,
+					bedInterval.getRefCpGList().size()));// internal bed is 0-based. Output bed end is 1-based.
 		}
+		System.out.println("median length: " + allIntervalList.stream()
+				.mapToInt(i -> i.getEnd() - i.getStart() + 1).sorted()
+				.toArray()[allIntervalList.size() / 2]);
+		System.out.println("median CpG number: " + allIntervalList.stream()
+				.mapToInt(i -> i.getRefCpGList().size()).sorted()
+				.toArray()[allIntervalList.size() / 2]);
 		writer.close();
 	}
 }
